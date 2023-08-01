1887cabec0b8a11f485ebdced2e8e1f2b1d0d3b6
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-08-01 14:43:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-window.promises = [];
-// Function to create a promise that resolves after a random time between 1 and 5 seconds
-function getRandomPromise() {
-  const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
-  return new Promise(resolve => {
+// window.promises = [];
+// // Function to create a promise that resolves after a random time between 1 and 5 seconds
+// function getRandomPromise() {
+//   const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(`Promise resolved after ${randomTime} milliseconds`);
+//     }, randomTime);
+//   });
+// }
+
+// // Array of five promises
+// const promises = [
+//   getRandomPromise(),
+//   getRandomPromise(),
+//   getRandomPromise(),
+//   getRandomPromise(),
+//   getRandomPromise()
+// ];
+
+// // Using Promise.any() to wait for the first promise to resolve
+// Promise.any(promises)
+//   .then(result => {
+//     const outputDiv = document.getElementById('output');
+//     outputDiv.textContent = result;
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+function createRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds in milliseconds
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(`Promise resolved after ${randomTime} milliseconds`);
+      resolve(`Promise resolved after ${Math.floor(randomTime / 1000)} seconds`);
     }, randomTime);
   });
 }
 
-// Array of five promises
-const promises = [
-  getRandomPromise(),
-  getRandomPromise(),
-  getRandomPromise(),
-  getRandomPromise(),
-  getRandomPromise()
+window.promises = [
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise()
 ];
 
-// Using Promise.any() to wait for the first promise to resolve
-Promise.any(promises)
-  .then(result => {
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = result;
+Promise.any(window.promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = result;
   })
-  .catch(error => {
-    console.error(error);
-  });
-
+  .catch((error) => console.error("Error:", error));
 
 
 // Do not change the code above this
